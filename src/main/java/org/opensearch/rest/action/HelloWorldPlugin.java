16640ac2d3953b5477fe73def6733ab8532bbfda
Bogdan <cool.b987@yandex.ru>
2023-06-20 15:47:02 +0300

    Добавлены Person и его CRUD

--- src/main/java/org/opensearch/rest/action/HelloWorldPlugin.java
+++ src/main/java/org/opensearch/rest/action/HelloWorldPlugin.java
@@ -7,45 +7,24 @@
  */
 package org.opensearch.rest.action;
 
-import org.opensearch.client.Client;
 import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.cluster.node.DiscoveryNodes;
-import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.io.stream.NamedWriteableRegistry;
 import org.opensearch.common.settings.ClusterSettings;
 import org.opensearch.common.settings.IndexScopedSettings;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.settings.SettingsFilter;
-import org.opensearch.common.xcontent.NamedXContentRegistry;
-import org.opensearch.env.Environment;
-import org.opensearch.env.NodeEnvironment;
 import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.Plugin;
-import org.opensearch.repositories.RepositoriesService;
 import org.opensearch.rest.RestController;
 import org.opensearch.rest.RestHandler;
-import org.opensearch.script.ScriptService;
-import org.opensearch.threadpool.ThreadPool;
-import org.opensearch.watcher.ResourceWatcherService;
+import org.opensearch.rest.action.resthandler.PersonIndexRestHandler;
+import org.opensearch.rest.action.resthandler.PersonRootRestHandler;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 
-import static java.util.Collections.singletonList;
-
 public class HelloWorldPlugin extends Plugin implements ActionPlugin {
-
-    @Override
-    public Collection<Object> createComponents(Client client, ClusterService clusterService, ThreadPool threadPool,
-                                               ResourceWatcherService resourceWatcherService, ScriptService scriptService,
-                                               NamedXContentRegistry xContentRegistry, Environment environment,
-                                               NodeEnvironment nodeEnvironment, NamedWriteableRegistry namedWriteableRegistry,
-                                               IndexNameExpressionResolver indexNameExpressionResolver,
-                                               Supplier<RepositoriesService> repositoriesServiceSupplier) {
-        return Collections.emptyList();
-    }
 
     @Override
     public List<RestHandler> getRestHandlers(final Settings settings,
@@ -56,6 +35,9 @@ public class HelloWorldPlugin extends Plugin implements ActionPlugin {
                                              final IndexNameExpressionResolver indexNameExpressionResolver,
                                              final Supplier<DiscoveryNodes> nodesInCluster) {
 
-        return singletonList(new RestHelloWorldAction());
+        return Arrays.asList(
+                new PersonIndexRestHandler(),
+                new PersonRootRestHandler()
+        );
     }
 }
